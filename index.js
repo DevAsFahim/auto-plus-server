66f518b35a52a96fba545bec712ac4d8d1f8d0d9
ASFahim007 <devasfahim@gmail.com>
2022-11-24 14:43:12 +0600

    mongodb basic satup

--- index.js
+++ index.js
@@ -10,6 +10,25 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.3q2ltkd.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.log)
+
+
+
+
 app.get('/', (req, res) => {
     res.send('welcome to auto plus server')
 })
