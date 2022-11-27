3552f3221f306df729133a9765762fa065383d66
ASFahim007 <devasfahim@gmail.com>
2022-11-27 19:01:09 +0600

    delete seller

--- index.js
+++ index.js
@@ -76,6 +76,15 @@ async function run() {
             const result = await usersCollection.updateOne(filter, updateDoc, options);
             res.send(result)
         })
+
+        // delete seller
+        app.delete('/seller/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
+        } )
+
     }
     finally {
 
